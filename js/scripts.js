974f5b6020b016651f43df98df95fe82d2192aed
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2023-12-04 12:42:08 +0100

    aggiornato con correzione

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 /*consegna
+0)  Never trust user input
 1)  Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 2)  Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
     a)  il prezzo del biglietto è definito in base ai km (0.21 € al km)
@@ -9,24 +10,28 @@
 
 
 //input dati
-const numeroKm = parseFloat(parseFloat(prompt("Inserisci il numero di chilometri da percorrere")));
-const eta = prompt("Inserisci la tua età");
-let scontoEta = 0;
-console.log("numerokm", numeroKm, typeof numeroKm)
+const numeroKm = parseInt(prompt("Inserisci il numero di chilometri da percorrere"));
+const eta = parseInt(prompt("Inserisci la tua età"));
+console.log("numero km", numeroKm, typeof numeroKm)
+console.log("età", eta, typeof eta)
+
 let prezzoBiglietto = numeroKm * 0.21;
 console.log("prezzo biglietto", prezzoBiglietto, typeof prezzoBiglietto)
-if (isNaN(numeroKm) || isNaN(eta)){
-    alert("Devi inserire un numero come età e come numero chilometri")
+
+if ((isNaN(numeroKm) || isNaN(eta)) && ((eta <= 130) && (eta >= 1))){
+    alert("Devi inserire un numero come età tra 1 e 130 e un numero come chilometri")
 }
 else{
     if (eta < 18) {
-        prezzoBiglietto=prezzoBiglietto*0.8;
+        prezzoBiglietto *= 0.8;
     }
-    else if (eta >=65){
-        prezzoBiglietto=prezzoBiglietto*0.6;
+    else if (eta >= 65){
+        prezzoBiglietto *= 0.6;
     }
     prezzoBiglietto = prezzoBiglietto.toFixed(2);
     console.log("prezzo biglietto scontato", prezzoBiglietto, typeof prezzoBiglietto)
     //output dati
-    document.querySelector(".risultato").innerHTML = "Il prezzo è il seguente " + (prezzoBiglietto);
+    document.querySelector(".risultato").innerHTML = "Il prezzo è il seguente: € " + (prezzoBiglietto);
+
 }
+
